1c9efc94f66f40a14d94562373ffc40580702ee0
simonmrog <simonmrog@gmail.com>
2023-08-23 18:44:10 -0500

    feat: basic jest, pre-commit, and linter configuration

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,6 @@ export default {
   transform: {
     "^.+\\.ts?$": "ts-jest",
   },
-  moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "<rootDir>/tests/__mocks__/styleMock.js",
-    "\\.(gif|ttf|eot|svg)$": "<rootDir>/tests/__mocks__/fileMock.js",
-  },
   transformIgnorePatterns: ["<rootDir>/node_modules/"],
   modulePathIgnorePatterns: [
     "src/index.tsx",
