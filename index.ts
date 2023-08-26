84b2581ae4746494421b8fd7a1e588c6fc76617c
simonmrog <simonmrog@gmail.com>
2023-08-26 14:58:03 -0500

    feat: server configuration

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World");
+import { initServer } from "./src/server";
+
+import config from "./src/config/config";
+
+const server = initServer();
+
+server.listen(config.PORT, () => console.log(`Server running on port ${config.PORT}`));
