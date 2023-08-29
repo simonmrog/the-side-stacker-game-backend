98d9de5e6e7040d95bbc80bb10eefd8dc9256426
simonmrog <simonmrog@gmail.com>
2023-08-29 00:15:28 -0500

    chore: adding GameState and GameStatus interfaces

--- src/services/sideStacker/sideStacker.interface.ts
+++ src/services/sideStacker/sideStacker.interface.ts
@@ -1,10 +1,22 @@
 export interface Game {
   start(): void;
-  restart(): void;
   endGame(result: string): void;
   handleTurn(player: string, move: Move): void;
   checkForDraw(): boolean;
   checkForWin(player: string, row: number, column: number): boolean;
+}
+
+export interface GameState {
+  status: GameStatus;
+  players: Array<string>;
+  board: Board;
+  currentPlayer: string;
+}
+
+export enum GameStatus {
+  NOT_STARTED = "not-started",
+  WAITING_FOR_SECOND_USER = "waiting-for-second-user",
+  STARTED = "started",
 }
 
 export type Board = Array<Row>;
