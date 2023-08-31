e0765c3aa0eedbd89afa2e9432179036008a23e1
simonmrog <simonmrog@gmail.com>
2023-08-30 21:21:43 -0500

    chore: error catalog

---
+++ src/config/errorCatalog.interface.ts
@@ -0,0 +1,7 @@
+export interface IErrorCatalog {
+  INVALID_MOVE: IInvalidMoveCatalog;
+}
+
+interface IInvalidMoveCatalog {
+  FULL_ROW: string;
+}
---
+++ src/config/errorCatalog.ts
@@ -0,0 +1,7 @@
+import { IErrorCatalog } from "./errorCatalog.interface";
+
+export const errorCatalog: IErrorCatalog = {
+  INVALID_MOVE: {
+    FULL_ROW: "INVALID_MOVE: The current row is full",
+  },
+};
