717396df427b3ee4486f608abe65b535cc7db9a0
simonmrog <simonmrog@gmail.com>
2023-09-05 13:11:06 -0500

    chore: removing unused controller

--- src/controllers/game.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-class GameController {
-  move(_req: Request, res: Response) {
-    res.status(200).send("good");
-  }
-}
-
-export default new GameController();
